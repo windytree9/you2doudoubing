c76019f1843eb39fdc25c142e800aecb4a0cc117
windytree9 <42682415+windytree9@users.noreply.github.com>
2018-08-31 17:20:07 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('SITE_NAME', '地藏王菩萨本愿经');
+define('TITLENAME', 'My Tube');
+define('EN2DEKEY', 'weeiwiei89/=;@');
+define('EMAIL', 'windytree9@outlook.com');
 ?>
